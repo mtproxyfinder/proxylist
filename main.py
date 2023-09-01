04d649c6a792b9da0a8fba63eae9a7876adb4637
alefvanoon <53198048+alefvanoon@users.noreply.github.com>
2023-09-01 21:40:07 +0000

    add check-host

--- main.py
+++ main.py
@@ -7,34 +7,47 @@ def get(json_link):
     mtproxy_list = json.loads(response.text)
     return mtproxy_list
 
+def check_broken_pipe(data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if check_broken_pipe(value):
+                return True
+    elif isinstance(data, list):
+        for item in data:
+            if check_broken_pipe(item):
+                return True
+    elif data == 'Broken pipe':
+        return True
+
+    return False
 def checkhost(addr):
     Result_URL =  "https://check-host.net/check-result/"
     Check_URL = "https://check-host.net/check-http"
     # Header for check-host.net 
     headers = {'Accept': 'application/json'}
     # Reaquest to check server
-    response = requests.get("https://check-host.net/check-http?host={addr}&node=ir1.node.check-host.net&node=ir2.node.check-host.net&node=ir3.node.check-host.net", headers=headers)
+    response = requests.get(f"https://check-host.net/check-http?host={addr}&node=ir1.node.check-host.net&node=ir2.node.check-host.net&node=ir3.node.check-host.net&node=ir4.node.check-host.net", headers=headers)
     request_id = json.loads(response.text)["request_id"]
     # load result
-    time.sleep(5)
+    time.sleep(10)
     headers = {'Accept': 'application/json'}
     result = requests.get(Result_URL + request_id, headers=headers)
     result = json.loads(result.text)
-    for server in result:
-        if result[server][0][2] == 'Connection refused':
-            return False
-        else:
-            return True
+    return check_broken_pipe(result)
 
 def save_mtproxy_list(mtproxy_list, file_name):
     with open(file_name, 'w') as file:
         for mtproxy in mtproxy_list:
             server = mtproxy['query']['server']
             port = mtproxy['query']['port']
+            print(server+":"+port)
             if checkhost(server+":"+port):
+                print("conected")
                 secret = mtproxy['query']['secret']
                 mtproxy_url = f"https://t.me/proxy?server={server}&port={port}&secret={secret}"
                 file.write(mtproxy_url + '\n')
+            else:
+                print("not conected")
 
 json_link = "https://raw.githubusercontent.com/yebekhe/MTProtoCollector/main/proxy/mtproto.json"  # Json link
 file_name = "list.txt"  # Name of the text file to save the mtproxy list
