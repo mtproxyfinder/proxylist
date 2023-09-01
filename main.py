c1d3059a14ea2bbb22523dd25474106133c62e56
alefvanoon <53198048+alefvanoon@users.noreply.github.com>
2023-09-01 17:38:04 +0330

    change end-point

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def get_mtproxy_list(json_link):
 def save_mtproxy_list(mtproxy_list, file_name):
     with open(file_name, 'w') as file:
         for mtproxy in mtproxy_list:
-            server = mtproxy['host']
-            port = mtproxy['port']
-            secret = mtproxy['secret']
+            server = mtproxy['query']['server']
+            port = mtproxy['query']['port']
+            secret = mtproxy['query']['secret']
             mtproxy_url = f"https://t.me/proxy?server={server}&port={port}&secret={secret}"
             file.write(mtproxy_url + '\n')
 
-json_link = "https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json"  # Replace with your json link
+json_link = "https://raw.githubusercontent.com/yebekhe/MTProtoCollector/main/proxy/mtproto.json"  # Json link
 file_name = "list.txt"  # Name of the text file to save the mtproxy list
 
 mtproxy_list = get_mtproxy_list(json_link)
