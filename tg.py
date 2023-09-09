333d20c7c054a91bbb71f497d1e3f43e3b1b332b
alefvanoon <53198048+alefvanoon@users.noreply.github.com>
2023-09-09 22:31:30 +0330

    Update tg.py

--- tg.py
+++ tg.py
@@ -1,15 +1,14 @@
 import os
-
 import requests
 import logging
 import random
 import time
 
-from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, Job
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, Job
 
 ChannelID = "-1001973476872"
-ChannelLink = "@mtproxyfinder" 
+ChannelLink = "@mtproxyfinder"
 
 # Enable logging
 logging.basicConfig(
@@ -20,15 +19,21 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+# Flag to track whether the proxy has been sent
+proxy_sent = False
+
 # get random line from url
 def get_random_line_from_url(url):
     response = requests.get(url)
     lines = response.text.splitlines()
     return random.choice(lines) if lines else None
 
-
-# send proxy every hour
+# send proxy only once
 async def send_proxy(context: ContextTypes.DEFAULT_TYPE) -> None:
+    global proxy_sent
+    if proxy_sent:
+        return
+
     url = "https://github.com/mtproxyfinder/proxylist/raw/main/list.txt"
     CH_ID = "-1001973476872"
     ADMIN_ID = "1301600392"
@@ -57,17 +62,15 @@ secret: {parsed_secret}
         ]
     )
 
-
     if proxy:
         # Send proxy to channel
         print("sending proxy")
         await context.bot.send_message(chat_id=CH_ID, text=text, reply_markup=reply_markup)
+        proxy_sent = True
     else:
         # send error to admin
         print("sending error to admin")
-        await context.bot.send_message(chat_id=ADMIN_ID, text="Error: no proxy found")    
-
-
+        await context.bot.send_message(chat_id=ADMIN_ID, text="Error: no proxy found")
 
 def main() -> None:
     BotToken = os.environ.get("TOKEN")
@@ -78,13 +81,12 @@ def main() -> None:
     # Add send proxy job to application
     application.job_queue.run_once(send_proxy, name="send_proxy", when=1)
     
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-    time.sleep(10)
-    
-    # Stop
-    application.stop_running()
+    # Run the bot until the user presses Ctrl-C or proxy_sent becomes True
+    while not proxy_sent:
+        try:
+            application.run_polling(allowed_updates=Update.ALL_TYPES)
+        except KeyboardInterrupt:
+            break
 
 if __name__ == "__main__":
     main()
